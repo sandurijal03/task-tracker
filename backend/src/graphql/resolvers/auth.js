93848d2de0cf7666d415ebaf55d22c711346e7d5
Sandip Rijal <sandurijal03@hotmail.com>
2021-10-30 13:03:20 +0545

    [server]: login function added in user resolvers

--- backend/src/graphql/resolvers/auth.js
+++ backend/src/graphql/resolvers/auth.js
@@ -1,3 +1,5 @@
+const { hashPassword, decryptPassword } = require('../../helpers/hash');
+
 const resolvers = {
   Query: {
     getUsers: () => 'get all users',
@@ -5,13 +7,41 @@ const resolvers = {
   Mutation: {
     register: async (parent, { userInput }, { User }, info) => {
       try {
-        const newUser = await new User({ ...userInput });
+        const { firstName, lastName, email, password } = userInput;
+        const hashedPassword = await hashPassword(password);
+        const newUser = await new User({
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+        });
 
         const user = await newUser.save();
         return user;
       } catch (err) {
         console.log(err);
         throw new Error('Error', err);
+      }
+    },
+
+    login: async (parent, { userInput }, { User }, info) => {
+      try {
+        const { email, password } = userInput;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new Error('User not found');
+        }
+
+        const isMatch = await decryptPassword(password, user.password);
+
+        if (!isMatch) {
+          throw new Error('Either email or password is invalid');
+        }
+        return user;
+      } catch (err) {
+        console.log(err.message);
+        throw new Error('error', err.message);
       }
     },
   },
