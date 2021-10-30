6ab0ab1f2fb2c727cdc67f43851f03b12035b5ff
Sandip Rijal <sandurijal03@hotmail.com>
2021-10-30 13:46:45 +0545

    [client]: all dev packages required to form react server is added

---
+++ frontend/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
