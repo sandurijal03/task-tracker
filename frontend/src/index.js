eaeb03a514192ee671f9158257ef717aba24466b
Sandip Rijal <sandurijal03@hotmail.com>
2021-10-30 13:55:35 +0545

    [client]: styled components added for the style of the sites and test app component is created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import styled from 'styled-components';
+
+const AppStyled = styled.div`
+  text-align: center;
+`;
+
+const App = () => {
+  return (
+    <AppStyled>
+      <h1>hello world</h1>
+    </AppStyled>
+  );
+};
+
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
