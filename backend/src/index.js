95d8f181487d9619645bb11515103f67ac560abc
Sandip Rijal <sandurijal03@hotmail.com>
2021-10-30 10:05:03 +0545

    [server]: test route hello added to test apollo server with required dependency graphql

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'hello world',
+  },
+};
+
 const mountServer = async () => {
-  app.get('/', (req, res) => {
-    res.status(200).json('test route added');
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs,
   });
+  await server.start();
+  server.applyMiddleware({ app, path: '/api' });
 
   app.listen(port, () => console.log(`[server]: listening on port ${port}`));
 };
