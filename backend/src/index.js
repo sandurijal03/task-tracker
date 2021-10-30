502a6203067459e9f8ef0cbecba342629e63646a
Sandip Rijal <sandurijal03@hotmail.com>
2021-10-30 12:21:27 +0545

    [server]: imported user model and passed through context to resolvers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 
 const { resolvers, typeDefs } = require('./graphql');
 const connectDB = require('./db/connectDB');
+const User = require('./models/User');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,6 +15,7 @@ const mountServer = async () => {
   const server = new ApolloServer({
     resolvers,
     typeDefs,
+    context: (req, res) => ({ req, res, User }),
   });
   await server.start();
   server.applyMiddleware({ app, path: '/api' });
