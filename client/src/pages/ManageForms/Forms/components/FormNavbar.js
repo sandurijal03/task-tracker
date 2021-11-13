957a7a33270ce64fda1f7573e432acd0baa72882
Sandip Rijal <sandurijal03@hotmail.com>
2021-11-13 18:08:27 +0545

    [client]: sidebar and header templates designed

--- client/src/pages/ManageForms/Forms/components/FormNavbar.js
+++ client/src/pages/ManageForms/Forms/components/FormNavbar.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  SaveOutlined,
+  EyeFilled,
+  EditFilled,
+  DeleteFilled,
+  CopyFilled,
+} from '@ant-design/icons';
 
 const FormNavbarStyled = styled.div`
   position: absolute;
-  left: 150px;
+  left: 80px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  /* height: 10px; */
+  box-shadow: 0 2px 4px rgba(42, 51, 66, 0.08);
+  width: calc(100% - 80px);
 
   img {
     position: absolute;
@@ -23,6 +34,46 @@ const FormNavbarStyled = styled.div`
   }
 `;
 
+const SaveFormButton = styled.button`
+  background-color: #5664d2;
+  color: white;
+  border-radius: 8px;
+  width: 100px;
+  padding: 5px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  text-transform: capitalize;
+  svg {
+    margin-right: 2px;
+  }
+`;
+
+const PreviewForm = styled(SaveFormButton)`
+  color: #5664d2;
+  background-color: white;
+  border: 1px solid #5664d2;
+  width: 120px;
+  padding: 5px;
+  border-radius: 8px;
+  svg {
+    margin-right: 2px;
+  }
+`;
+
+const UpdateFormStatus = styled(PreviewForm)`
+  width: 160px;
+`;
+
+const CopyForm = styled(PreviewForm)`
+  width: 100px;
+`;
+
+const DeleteForm = styled(PreviewForm)`
+  color: red;
+  border-color: red;
+`;
+
 const FormNavbar = () => {
   return (
     <FormNavbarStyled>
@@ -30,19 +81,34 @@ const FormNavbar = () => {
       <div className='right'>
         <ul>
           <li>
-            <button>save form</button>
+            <SaveFormButton>
+              <SaveOutlined />
+              save form
+            </SaveFormButton>
           </li>
           <li>
-            <button>Preview form</button>
+            <PreviewForm>
+              <EyeFilled />
+              Preview form
+            </PreviewForm>
           </li>
           <li>
-            <button>Update form status</button>
+            <UpdateFormStatus>
+              <EditFilled />
+              Update form status
+            </UpdateFormStatus>
           </li>
           <li>
-            <button>Copy form</button>
+            <CopyForm>
+              <CopyFilled />
+              Copy form
+            </CopyForm>
           </li>
           <li>
-            <button>Delete form</button>
+            <DeleteForm>
+              <DeleteFilled />
+              Delete form
+            </DeleteForm>
           </li>
         </ul>
       </div>
--- client/src/pages/ManageForms/Forms/components/MessageSidebar.js
+++ client/src/pages/ManageForms/Forms/components/MessageSidebar.js
@@ -9,56 +9,82 @@ import { VscChecklist, VscServerProcess } from 'react-icons/vsc';
 import { GrUserAdmin } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 
-const MessageSidebarStyled = styled.ul`
+const MessageSidebarStyled = styled.div`
   position: absolute;
-  left: 10px;
-  list-style: none;
-  width: 120px;
-  border: 2px solid;
+  left: 100px;
+  width: 80px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 100%;
+  box-shadow: 0 2px 4px rgba(42, 51, 66, 0.08);
+  /* z-index: 1000; */
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+    font-weight: bold;
+    span {
+      font-size: 0.8rem;
+    }
+    :hover,
+    :active {
+      color: blue;
+    }
+  }
 
   li {
-    border: 2px solid;
-    padding: 10px;
-    margin: 10px;
-    :active {
-    color: blue;
-    }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 2px solid lightgray;
+    text-align: center;
+    padding: 10px 10px;
+    width: 80px;
+    height: 80px;
   }
 `;
 
 const MessageSidebar = () => {
   return (
     <MessageSidebarStyled>
-      <li>
-        <Link to='manage-forms/form/sample/edit/:id/settings'>
-          <AiFillSetting /> Settings
-        </Link>
-      </li>
-      <li>
-        <Link to='manage-forms/form/sample/edit/:id/designer'>
-          <AiOutlineAntDesign /> Designer
-        </Link>
-      </li>
-      <li>
-        <Link to='manage-forms/form/sample/edit/:id/process'>
-          <VscServerProcess /> Process
-        </Link>
-      </li>
-      <li>
-        <Link to='manage-forms/form/sample/edit/:id/permission'>
-          <GrUserAdmin /> Permission
-        </Link>
-      </li>
-      <li>
-        <Link to='manage-forms/form/sample/edit/:id/reports'>
-          <VscChecklist /> Reports
-        </Link>
-      </li>
-      <li>
-        <Link to='manage-forms/forms'>
-          <AiOutlineRollback /> Back to forms list
-        </Link>
-      </li>
+      <ul>
+        <li>
+          <Link to='manage-forms/form/sample/edit/:id/settings'>
+            <AiFillSetting /> Settings
+          </Link>
+        </li>
+        <li>
+          <Link to='manage-forms/form/sample/edit/:id/designer'>
+            <AiOutlineAntDesign /> Designer
+          </Link>
+        </li>
+        <li>
+          <Link to='manage-forms/form/sample/edit/:id/process'>
+            <VscServerProcess /> Process
+          </Link>
+        </li>
+        <li>
+          <Link to='manage-forms/form/sample/edit/:id/permission'>
+            <GrUserAdmin /> Permission
+          </Link>
+        </li>
+        <li>
+          <Link to='manage-forms/form/sample/edit/:id/reports'>
+            <VscChecklist /> Reports
+          </Link>
+        </li>
+        <li>
+          <Link to='manage-forms/forms'>
+            <AiOutlineRollback /> Back to forms list
+          </Link>
+        </li>
+      </ul>
     </MessageSidebarStyled>
   );
 };
